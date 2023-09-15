d045af5c1c7253e5b0a702342a8aa0caaa835240
Lê Minh Bảo <90511030+leminhbao308@users.noreply.github.com>
2023-09-15 18:21:43 +0700

    Uploading Src Files

---
+++ src/main/java/cat_std/broseidon/mc_tutorial/MC_Tutorial.java
@@ -0,0 +1,16 @@
+package cat_std.broseidon.mc_tutorial;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MC_Tutorial extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Method này sẽ được gọi khi Plugin được bật
+    }
+
+    @Override
+    public void onDisable() {
+        // Method này sẽ được gọi khi Plugin được tắt
+    }
+}
